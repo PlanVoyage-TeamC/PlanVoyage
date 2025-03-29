1ffbfdbedeabf7b969fe3e47f42dfdb18699e97a
Aakanksha-Rangdal <arrangdal@gmail.com>
2025-03-29 15:40:59 -0400

    Created necessary components and built the home page

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-center text-xl font-light text-white  p-4 space-x-20">
+      <a href="/destinations" className="">
+        Destinations
+      </a>
+      <p className="">About</p>
+      <p className="">Conatct Us</p>
+    </div>
+  );
+}
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import Logo from "../../../public/images/logo.png";
+import Navbar from "../components/Navbar";
+import SignInButton from "../components/signInButton";
+import Footer from "../components/Footer";
+
 export default function Home() {
-    return (
-      <div className="">
-      Start Login page here
+  return (
+    <div className="homeBg w-full h-screen relative">
+      <div className="bg-[#3A2C2285] w-full h-full">
+        <Image src={Logo} alt="Logo" width={200} height={200} className="" />
+        <div className="">
+          <Navbar />
+        </div>
+        <div className="h-screen items-center text-center mt-16">
+          <div className="font-extrabold text-7xl">START EXPLORING,</div>
+          <div className="font-extrabold text-7xl">TODAY</div>
+          <div className="">
+            <SignInButton />
+          </div>
+        </div>
       </div>
-    );
-  }
+      <Footer />
+    </div>
+  );
+}
