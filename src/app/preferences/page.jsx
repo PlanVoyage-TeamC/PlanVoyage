6c001667433dacd8e08b34f14efd20b6bd104f5d
Aakanksha-Rangdal <arrangdal@gmail.com>
2025-03-29 20:08:44 -0400

    started off with preference page

--- src/app/preferences/page.jsx
+++ src/app/preferences/page.jsx
@@ -1,8 +1,16 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 export default function Preferences() {
-    return (
-      <div className="">
-        Start preferences page here
+  return (
+    <div className="preferencesBg w-full h-screen relative ">
+      <div className="flex items-center justify-center text-2xl">Fill this one time preference form to get better recommendations!</div>
+      <div className="w-9/10 h-9/10 rounded-2xl bg-[#ffffff80] my-2 mx-15 flex items-center justify-center ">
+        <div className="  ">hello</div>
       </div>
-    );
-  }
-  
+
+      <div className="">
+        <Footer />
+      </div>
+    </div>
+  );
+}
